82e408a2d2b250153477f082c9e4a6cbc645852c
Ruben <38133978+Bubba1986@users.noreply.github.com>
2023-01-17 18:10:55 +0200

    Initial upload
    Initial upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function removeDash() {
+  var input = document.getElementById("text-input").value;
+  var output = input.replace(/-/g, " ");
+  document.getElementById("output").innerHTML = output;
+}
+
+function copyOutput() {
+  var output = document.getElementById("output").innerHTML;
+  navigator.clipboard.writeText(output).then(function() {
+    console.log("Copied to clipboard");
+  }, function(err) {
+    console.error("Failed to copy: ", err);
+  });
+}
